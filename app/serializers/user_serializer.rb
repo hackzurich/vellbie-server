4e840e263f0077ffdd9c9966b86062d9476ded91
AlexIv <alx.ekb@gmail.com>
2021-09-26 02:14:55 +0500

    Add user stress stats

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -7,4 +7,12 @@ class UserSerializer
   attribute :goals do |record|
     record.goals.map { |goal| { title: goal.title, id: goal.id } }
   end
+
+  attribute :meta do |record|
+    {
+      max_stress_level: record.stress_logs.maximum(:stress_level),
+      average_stress_level: record.stress_logs.average(:stress_level).to_i,
+      min_stress_level: record.stress_logs.minimum(:stress_level)
+    }
+  end
 end
